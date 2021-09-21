2527c83fbcaa40641c19a8e092a0c539391f823f
jnzhihuoo1 <593757535@qq.com>
2021-09-21 09:56:14 +0800

    initalize

--- server/setup.py
+++ server/setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='gnnvis',
+    name='gnnlens',
     version='0.0.0',
     url='http://flask.pocoo.org/docs/tutorial/',
     install_requires=[
         'flask',
     ],
+    packages=find_packages(include=['gnnlens', 'gnnlens.*']),
+    package_data={'gnnlens': ['visbuild/*', 'visbuild/static/js/*', 'visbuild/static/css/*', 'visbuild/static/media/*']},
     entry_points={
         'console_scripts': [
-            'gnnvis=gnn_vis:cli'
+            'gnnlens=gnnlens.server:start_server'
         ],
     },
 )
